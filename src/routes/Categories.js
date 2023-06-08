5345f19d316e477a1b15481790b1aaec0b2a8262
andrianarivo <andrianarivo.david@gmail.com>
2023-06-08 17:46:10 +0300

    build routes

---
+++ src/routes/Categories.js
@@ -0,0 +1,7 @@
+export default function Categories() {
+  return (
+    <div>
+      <p>This is the Categories page</p>
+    </div>
+  );
+}
---
+++ src/routes/Home.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <p>This is the Home Page</p>
+    </div>
+  );
+}
