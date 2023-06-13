d5903393a171cb4de114099bdfe026da34675a98
andrianarivo <andrianarivo.david@gmail.com>
2023-06-13 13:40:03 +0300

    fix delete item bug

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -3,19 +3,19 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   bookItems: [
     {
-      item_id: 'item1',
+      id: 'item1',
       title: 'The Great Gatsby',
       author: 'John Smith',
       category: 'Fiction',
     },
     {
-      item_id: 'item2',
+      id: 'item2',
       title: 'Anna Karenina',
       author: 'Leo Tolstoy',
       category: 'Fiction',
     },
     {
-      item_id: 'item3',
+      id: 'item3',
       title: 'The Selfish Gene',
       author: 'Richard Dawkins',
       category: 'Nonfiction',
